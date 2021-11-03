ff542109a5dda11c032158bfebff6a28a7d2daf8
Joao Lopes <joaorfclopes@gmail.com>
2021-11-03 18:43:24 +0000

    icon added to navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 import styles from '../../styles/Navbar.module.css'
 
@@ -5,7 +6,9 @@ export default function Navbar() {
   return (
     <div className={styles.navbar}>
       <div className={styles.container}>
-        <div className={styles.element}>Left</div>
+        <div className={styles.element}>
+          <Image src="/note.svg" width="40px" height="40px" />
+        </div>
         <div className={styles.element}>Right</div>
       </div>
     </div>
