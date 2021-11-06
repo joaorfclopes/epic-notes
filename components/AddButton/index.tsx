94a36fcd2503a9330dbabadc846b4265cbb989a8
Joao Lopes <joaorfclopes@gmail.com>
2021-11-06 18:23:12 +0000

    links added to buttons

--- components/AddButton/index.tsx
+++ components/AddButton/index.tsx
@@ -1,11 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 import Add from '../../icons/Add'
 import styles from '../../styles/AddButton.module.css'
 
 export default function AddButton() {
   return (
-    <div className={styles.add}>
-      <Add />
-    </div>
+    <Link href="/notes/create">
+      <div className={styles.add}>
+        <Add />
+      </div>
+    </Link>
   )
 }
