78a7f212aaf3d1178429c8928baf25397f44c9cb
Joao Lopes <joaorfclopes@gmail.com>
2021-11-08 12:25:46 +0000

    Loading added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
 import type { AppProps } from 'next/app'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  Router.events.on('routeChangeStart', (url: string) => {
+    NProgress.start()
+  })
+
+  Router.events.on('routeChangeComplete', (url: string) => {
+    NProgress.done()
+  })
+
   return (
     <Layout>
+      <link rel="stylesheet" href="/nprogress.css" />
       <Component {...pageProps} />
     </Layout>
   )
