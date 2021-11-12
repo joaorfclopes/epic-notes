098edb4ce1650abd8a3e20a99a41f96707ba488c
Joao Lopes <joaorfclopes@gmail.com>
2021-11-12 20:13:42 +0000

    pwa fix - console changed

--- public/service_worker_script.js
+++ public/service_worker_script.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('sw.js')
     .then(function (registration) {
-      console.log('success load')
+      console.log('service worker loaded')
     })
     .catch(function (err) {
       console.log(err)
