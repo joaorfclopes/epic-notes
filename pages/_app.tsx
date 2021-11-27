0addd492548755547696aaa6ff914f77f5517cae
Joao Lopes <joaorfclopes@gmail.com>
2021-11-27 12:54:49 +0000

    service_worker_script type fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Script src="/service_worker_script.js" />
+      <Script type="text/html" src="/service_worker_script.js" />
       <Layout>
         <Component {...pageProps} />
       </Layout>
