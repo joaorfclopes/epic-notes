6ef0b244bd49f4140fd55f65cac9c448b4d77aab
Joao Lopes <joaorfclopes@gmail.com>
2021-11-29 23:59:01 +0000

    last note added appears first ✅

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,8 @@ function Index(props: Props) {
 
 export async function getServerSideProps() {
   const res = await fetch(process.env.API_URL as string)
-  const notes = await res.json()
+  const notesArray = await res.json()
+  const notes = notesArray.reverse()
 
   return {
     props: { notes }
